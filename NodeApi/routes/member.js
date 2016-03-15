a40bd6dd977e7781cf165516bfbfcede817fb70d
Gallan Probo Widyanto <gallan.widyanto@gmail.com>
2016-03-15 12:15:39 +0700

    edit insert, update query

--- NodeApi/routes/member.js
+++ NodeApi/routes/member.js
@@ -34,7 +34,7 @@ router.post("/",function(req,res,next) {
 				console.log(err);
 			}else{
 				if(rows[0].result===1){
-					connection.query('insert into member (id_member, identity_number, created_at, updated_at, npwp, total_komisi, total_saldo) values ("'+req.body.id_member+'" , "'+req.body.identity_number+'" , "'+req.body.created_at+'" , "'+req.body.updated_at+'" , "'+req.body.npwp+'" , "'+req.body.total_komisi+'" , "'+req.body.total_saldo+'")', function(err, rows, fields) {
+					connection.query('insert into member (identity_number, npwp, level_member) values ("'+req.body.identity_number+'" , "'+req.body.npwp+'" , "'+req.body.level_member+'")', function(err, rows, fields) {
 						if (err){
 						   console.log(err);
 						}else{
@@ -62,7 +62,7 @@ router.put("/",function(req,res,next) {
 				console.log(err);
 			}else{
 				if(rows[0].result===1){
-					connection.query('update member set identity_number="'+req.body.identity_number+'", created_at="'+req.body.created_at+'", updated_at="'+req.body.updated_at+'", npwp="'+req.body.npwp+'", total_komisi="'+req.body.total_komisi+'", total_saldo="'+req.body.total_saldo+'" where (id_member="'+req.body.id_member+'")', function(err, rows, fields) {
+					connection.query('update member set identity_number="'+req.body.identity_number+'", updated_at="'+req.body.updated_at+'", npwp="'+req.body.npwp+'", total_komisi="'+req.body.total_komisi+'", total_saldo="'+req.body.total_saldo+'" where (id_member="'+req.body.id_member+'")', function(err, rows, fields) {
 						if (err){
 						   console.log(err);
 						}else{
